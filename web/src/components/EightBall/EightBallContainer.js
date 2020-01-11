de1aea8a8ad76326793b84741de7e250cc1a48cf
jpanuncialman <joep@hzdg.com>
2020-01-11 18:05:30 -0500

    Test shake 5

--- web/src/components/EightBall/EightBallContainer.js
+++ web/src/components/EightBall/EightBallContainer.js
@@ -31,7 +31,7 @@ export default class EightBallContainer extends Component {
         // var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
         
         this.renderer = new THREE.WebGLRenderer({antialias: true});
-        this.renderer.setSize( this.mount.clientWidth, this.mount.clientHeight);
+        this.renderer.setSize( window.innerWidth, window.innerHeight);
         this.renderer.setClearColor('#000000')
         // renderer.setSize( this.mount.clientWidth, this.mount.clientHeight );
         this.mount.appendChild( this.renderer.domElement );
@@ -40,8 +40,8 @@ export default class EightBallContainer extends Component {
         // var cube = new THREE.Mesh( geometry, material );
         // scene.add( cube );
         // camera.position.z = 5;
-        this.camera = new THREE.PerspectiveCamera(75,this.mount.clientWidth/this.mount.clientHeight,0.1,1000);
-        // this.camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
+        // this.camera = new THREE.PerspectiveCamera(75,this.mount.clientWidth/this.mount.clientHeight,0.1,1000);
+        this.camera = new THREE.PerspectiveCamera(75,window.innerWidth / window.innerHeight,0.1,1000);
         // camera.rotation.y = 45/180*Math.PI;
         this.camera.position.x = 0;
         this.camera.position.y = 0;
