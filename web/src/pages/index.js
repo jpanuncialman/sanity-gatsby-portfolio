dac25b11f44fc4c2697b81fb1a4ef9b077e5726b
jpanuncialman <joep@hzdg.com>
2020-01-11 18:26:40 -0500

    Test shake 9

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -40,7 +40,7 @@ const shakeEventDidOccur = () => {
   // alert('shake!');
 
     // Shake sensitivity (a lower number is more)
-    var sensitivity = 20;
+    var sensitivity = 25;
 
     // Position variables
     var x1 = 0, y1 = 0, z1 = 0, x2 = 0, y2 = 0, z2 = 0;
